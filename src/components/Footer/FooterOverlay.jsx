8b9b8ed12772f595eca140d765a6d7b15e6b8536
saeed <saeed@mary.local>
2023-05-12 14:22:33 +0330

    completing footer by changing FooterOverlay and Newsletter and also footer's components and styles

--- src/components/Footer/FooterOverlay.jsx
+++ src/components/Footer/FooterOverlay.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './FooterOverlay.css';
+import "./FooterOverlay.css";
 
-const FooterOverlay = () => (
-  <div>
-    FooterOverlay
-  </div>
-);
+const FooterOverlay = () => {
+  return (
+    <div className="app__footerOverlay">
+      <div className="app__footerOverlay-black"></div>
+      <div className="app__footerOverlay-img app__bg"></div>
+    </div>
+  );
+};
 
 export default FooterOverlay;
--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import "./Newsletter.css";
+import SubHeading from "../SubHeading/SubHeading";
 
-import './Newsletter.css';
-
-const Newsletter = () => (
-  <div>
-    Newsletter
-  </div>
-);
+const Newsletter = () => {
+  return (
+    <div className="app__newsletter">
+      <div className="app__newsletter-heading">
+        <SubHeading title="News Letter" />
+        <h1 className="headtext__cormorant">Subscribe To Our Newsletter</h1>
+        <p className="p__opensans">And never miss latest Updates!</p>
+      </div>
+      <div className="app__newsletter-input flex__center">
+        <input type="email" placeholder="Enter your e-mail address" />
+        <button type="button" className="custom__button">
+          Subscribe
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Newsletter;
